e85222b689d9be1bbcea980a920cce5842550574
youness <younesshamou013@gmail.com>
2026-03-04 23:53:58 +0000

    feat(database): setup core schema for users,categories

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,12 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('role_id')->constrained('roles');
+
+            $table->decimal('budget_min', 10, 2)->unsigned()->default(0);
+            $table->decimal('budget_max', 10, 2)->unsigned()->default(0);
+            
+            $table->text('interests')->json('interests')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
